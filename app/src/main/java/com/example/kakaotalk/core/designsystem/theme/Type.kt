03dccb91a5b8382bd91dbef8486cc396e4b4957b
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-05-11 23:21:02 +0900

    [chore#3] Type 내부 FontFamily 변수 오탈자 수정 및 head4 fontsize 수정 완료

--- app/src/main/java/com/example/kakaotalk/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/example/kakaotalk/core/designsystem/theme/Type.kt
@@ -1,6 +1,5 @@
 package com.example.kakaotalk.core.designsystem.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
@@ -10,7 +9,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.kakaotalk.R
-import com.example.kakaotalk.core.designsystem.theme.PretendartFontFamily
 
 @Immutable
 data class KakaoTalkTypography(
@@ -25,64 +23,64 @@ data class KakaoTalkTypography(
     val head4: TextStyle
 )
 
-private val PretendartFontFamily = FontFamily(
+private val PretendardFontFamily = FontFamily(
     Font(R.font.pretendard_medium, weight = FontWeight.Normal),
     Font(R.font.pretendard_bold, weight = FontWeight.Bold),
 )
 
 val defaultKakaoTalkTypography = KakaoTalkTypography(
     body1 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         letterSpacing = 0.01.em
     ),
     body2 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 15.sp,
         letterSpacing = 0.01.em
     ),
     body3 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 13.sp,
         letterSpacing = 0.01.em
     ),
     body4 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         letterSpacing = 0.01.em
     ),
     body5 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
         letterSpacing = 0.01.em
     ),
     head1 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         letterSpacing = 0.05.em
     ),
     head2 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         letterSpacing = 0.05.em
     ),
     head3 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
         letterSpacing = 0.05.em
     ),
     head4 = TextStyle(
-        fontFamily = PretendartFontFamily,
+        fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Bold,
-        fontSize = 22.sp,
+        fontSize = 12.sp,
         letterSpacing = 0.05.em
     )
 )
