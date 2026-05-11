15b659a1603de6dc6525d557200b3b4358f144a4
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-05-11 16:48:00 +0900

    [feat#3] theme-color 파일(컬러시스템) 구축

--- app/src/main/java/com/example/kakaotalk/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/example/kakaotalk/core/designsystem/theme/Color.kt
@@ -1,11 +1,37 @@
 package com.example.kakaotalk.core.designsystem.theme
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+@Immutable
+object KakaoTalkColors {
+    // gray
+    val gray900 = Color(0xFF393939)
+    val gray800 = Color(0xFF4D4D4D)
+    val gray700 = Color(0xFF595959)
+    val gray600 = Color(0xFF737373)
+    val gray500 = Color(0xFFB6B6B6)
+    val gray400 = Color(0xFFCDCDCD)
+    val gray300 = Color(0xFFDBDBDB)
+    val gray200 = Color(0xFFF0F0F0)
+    val gray100 = Color(0xFFF5F5F5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    // blue
+    val blue300 = Color(0xFF2F363E)
+    val blue200 = Color(0xFF4A535A)
+    val blue100 = Color(0xFFB8BCBF)
+
+    // basic
+    val white = Color(0xFFFFFFFF)
+    val black = Color(0xFF191919)
+
+    // yellow
+    val yellow400 = Color(0xFFFFF9C4)
+    val yellow500 = Color(0xFFFEE500)
+    val yellow600 = Color(0xFFF6DE00)
+
+    // orange
+    val orange300 = Color(0xFFEA5B21)
+    val orange200 = Color(0xFFFF9117)
+    val orange100 = Color(0xFFFDEEE7)
+}
