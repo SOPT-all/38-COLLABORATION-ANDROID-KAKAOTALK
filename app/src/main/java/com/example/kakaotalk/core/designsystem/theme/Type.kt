4c5cb6518f8ecf49d20829d76da929dd6f90d5d0
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-05-11 16:48:25 +0900

    [feat#3] theme-type 파일(타이포그래피) 구축

--- app/src/main/java/com/example/kakaotalk/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/example/kakaotalk/core/designsystem/theme/Type.kt
@@ -1,34 +1,87 @@
 package com.example.kakaotalk.core.designsystem.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.example.kakaotalk.R
+import com.example.kakaotalk.core.designsystem.theme.PretendartFontFamily
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+@Immutable
+data class KakaoTalkTypography(
+    val body1: TextStyle,
+    val body2: TextStyle,
+    val body3: TextStyle,
+    val body4: TextStyle,
+    val body5: TextStyle,
+    val head1: TextStyle,
+    val head2: TextStyle,
+    val head3: TextStyle,
+    val head4: TextStyle
+)
+
+private val PretendartFontFamily = FontFamily(
+    Font(R.font.pretendard_medium, weight = FontWeight.Normal),
+    Font(R.font.pretendard_bold, weight = FontWeight.Bold),
+)
+
+val defaultKakaoTalkTypography = KakaoTalkTypography(
+    body1 = TextStyle(
+        fontFamily = PretendartFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.01.em
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+    body2 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        letterSpacing = 0.01.em
+    ),
+    body3 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        letterSpacing = 0.01.em
+    ),
+    body4 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        letterSpacing = 0.01.em
+    ),
+    body5 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.01.em
+    ),
+    head1 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        letterSpacing = 0.05.em
+    ),
+    head2 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        letterSpacing = 0.05.em
+    ),
+    head3 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.05.em
+    ),
+    head4 = TextStyle(
+        fontFamily = PretendartFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        letterSpacing = 0.05.em
     )
-    */
 )
