7a58ebdafaf78e6dde12ccb4d31ee1dc032ce782
lmnoo <doorimng@gmail.com>
2026-05-12 22:29:43 +0900

    [feat/#6] TopAppBar onClick 구현

--- app/src/main/java/com/example/kakaotalk/core/designsystem/component/KakaoTopAppBar.kt
+++ app/src/main/java/com/example/kakaotalk/core/designsystem/component/KakaoTopAppBar.kt
@@ -1,50 +1,39 @@
 package com.example.kakaotalk.core.designsystem.component
 
-import android.R.attr.onClick
-import android.R.attr.text
-import android.R.attr.textColor
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kakaotalk.R
+import com.example.kakaotalk.core.common.extension.noRippleClickable
 import com.example.kakaotalk.core.designsystem.theme.KakaoTheme
-
-
 
 @Composable
 fun KakaoTopAppBar(
-    text: String = "채팅방 폴더 관리",
     modifier: Modifier = Modifier,
-    onClick: () -> Unit = {},
+    text: String = "채팅방 폴더 관리",
     color: Color = KakaoTheme.colors.black,
-    isSuccessVisible: Boolean ?= false
+    onBackClick: () -> Unit = {},
+    onSuccessClick: () -> Unit = {},
+    isSuccessVisible: Boolean = false
 ){
 
     Row(
         modifier = modifier
             .fillMaxWidth()
             .background(Color.White)
-            .clickable(onClick = onClick)
             .padding(vertical = 12.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -53,7 +42,8 @@ fun KakaoTopAppBar(
             contentDescription = null,
             modifier = Modifier
                 .padding(end = 16.dp)
-                .size(25.dp),
+                .size(25.dp)
+                .noRippleClickable(onClick = onBackClick),
             tint = color
         )
 
@@ -63,11 +53,12 @@ fun KakaoTopAppBar(
             style = KakaoTheme.typography.head2
         )
         
-        if (isSuccessVisible == true) {
+        if (isSuccessVisible) {
             Spacer(modifier = Modifier.weight(1f))
 
             Text(
                 text = "완료",
+                modifier = Modifier.noRippleClickable(onClick = onSuccessClick),
                 color = color,
                 style = KakaoTheme.typography.body2
             )
@@ -81,7 +72,9 @@ private fun KakaoTopAppBarPreview(){
     KakaoTheme {
         Column(){
             KakaoTopAppBar()
-            Spacer(Modifier.padding(10.dp))
+
+            Spacer(modifier = Modifier.size(10.dp))
+
             KakaoTopAppBar(
                 text = "폴더 편집",
                 isSuccessVisible = true
