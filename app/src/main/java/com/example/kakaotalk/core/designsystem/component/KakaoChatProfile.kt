2738f737a5c7f06d93a2a0ac4146d62a365239c2
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-05-12 20:20:20 +0900

    (chore#10) 불필요한 import 제거

--- app/src/main/java/com/example/kakaotalk/core/designsystem/component/KakaoChatProfile.kt
+++ app/src/main/java/com/example/kakaotalk/core/designsystem/component/KakaoChatProfile.kt
@@ -2,14 +2,9 @@ package com.example.kakaotalk.core.designsystem.component
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
