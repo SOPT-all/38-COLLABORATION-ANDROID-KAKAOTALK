beff564aa63780b3fa01f40a8b8db046b51b03bf
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-05-13 21:28:38 +0900

    (chore#16) TopBar Height 지정 (33.dp)

--- app/src/main/java/com/example/kakaotalk/presentation/chatlist/component/ChatListTopBar.kt
+++ app/src/main/java/com/example/kakaotalk/presentation/chatlist/component/ChatListTopBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +23,7 @@ private fun ChatListTopBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .height(33.dp)
             .background(color = KakaoTheme.colors.white),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
