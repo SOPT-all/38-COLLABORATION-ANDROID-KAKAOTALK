f9e24a3e09df96cac79037b21ff543c6c809027f
doorimng <doorimng@gmail.com>
2026-05-13 23:58:10 +0900

    [feat/#23] 추천 폴더 추가 아이템 UI 구현

---
+++ app/src/main/java/com/example/kakaotalk/core/designsystem/component/KakaoFolderAddItem.kt
@@ -0,0 +1,107 @@
+package com.example.kakaotalk.core.designsystem.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kakaotalk.R
+import com.example.kakaotalk.core.designsystem.theme.KakaoTheme
+@Composable
+fun KakaoFolderAddItem(
+    text: String,
+    @DrawableRes icon: Int,
+    modifier: Modifier = Modifier,
+    iconColor: Color = KakaoTheme.colors.black
+){
+    Box(
+        modifier = modifier
+            .width(328.dp)
+            .height(30.dp)
+            .background(KakaoTheme.colors.white)
+    ) {
+        Row(
+            modifier = Modifier.align(Alignment.CenterStart),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(28.dp)
+                    .background(
+                        color = KakaoTheme.colors.gray200,
+                        shape = CircleShape
+                    ),
+                tint = iconColor
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = text,
+                style = KakaoTheme.typography.body1
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .width(45.dp)
+                .height(30.dp)
+                .border(
+                    width = 1.dp,
+                    color = KakaoTheme.colors.gray300,
+                    shape = RoundedCornerShape(999.dp)
+                ),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = "추가",
+                style = KakaoTheme.typography.body4,
+                color = KakaoTheme.colors.black
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun KakaoFolderAddItemPreview(){
+    KakaoTheme{
+        Column(){
+            KakaoFolderAddItem(
+                text = "ChatGPT",
+                icon = R.drawable.ic_gpt_24,
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            KakaoFolderAddItem(
+                text = "즐겨찾기",
+                icon = R.drawable.ic_star_24,
+                iconColor = KakaoTheme.colors.orange200
+            )
+        }
+
+    }
+}
