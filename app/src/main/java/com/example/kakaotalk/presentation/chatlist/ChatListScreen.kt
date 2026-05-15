96292b6f78081d9edc10a97032ec17913d87caf1
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-05-16 01:51:29 +0900

    (chore#41) 안쓰는 임포트 제거

--- app/src/main/java/com/example/kakaotalk/presentation/chatlist/ChatListScreen.kt
+++ app/src/main/java/com/example/kakaotalk/presentation/chatlist/ChatListScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
