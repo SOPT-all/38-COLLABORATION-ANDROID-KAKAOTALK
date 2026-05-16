7e8c75a9a05f034a842c62beaf877ab9685e584d
doorimng <doorimng@gmail.com>
2026-05-16 13:14:06 +0900

    [refactor/#13] 사용하지 않는 import 삭제

--- app/src/main/java/com/example/kakaotalk/presentation/folder/folderedit/component/KakaoFolderSelectItem.kt
+++ app/src/main/java/com/example/kakaotalk/presentation/folder/folderedit/component/KakaoFolderSelectItem.kt
@@ -1,14 +1,11 @@
 package com.example.kakaotalk.presentation.folder.folderedit.component
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
