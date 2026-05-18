33943a10ce8f1dc01768e641015c60e304ec9940
doorimng <doorimng@gmail.com>
2026-05-19 00:54:01 +0900

    [refactor/#46] 불필요한 import문 삭제

--- app/src/main/java/com/example/kakaotalk/presentation/folder/folderhome/FolderHomeScreen.kt
+++ app/src/main/java/com/example/kakaotalk/presentation/folder/folderhome/FolderHomeScreen.kt
@@ -2,19 +2,13 @@ package com.example.kakaotalk.presentation.folder.folderhome
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import com.example.kakaotalk.core.designsystem.theme.KakaoTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
