e0b2c2a3e85d15b73d1789170d4719382d5359eb
chanmi <cksal1052@ewha.ac.kr>
2026-05-20 08:57:42 +0900

    [Refactor/#46] KakaoTopAppBar 수정

--- app/src/main/java/com/example/kakaotalk/core/designsystem/component/KakaoTopAppBar.kt
+++ app/src/main/java/com/example/kakaotalk/core/designsystem/component/KakaoTopAppBar.kt
@@ -1,6 +1,5 @@
 package com.example.kakaotalk.core.designsystem.component
 
-import android.R.attr.text
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
