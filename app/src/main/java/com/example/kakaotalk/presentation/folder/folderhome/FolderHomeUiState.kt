c522f166f45dbb6c23cb02c3e88917428716a566
doorimng <doorimng@gmail.com>
2026-05-19 22:30:42 +0900

    [feat/#47] FolderHome 뷰모델, UiState 구현

---
+++ app/src/main/java/com/example/kakaotalk/presentation/folder/folderhome/FolderHomeUiState.kt
@@ -0,0 +1,6 @@
+package com.example.kakaotalk.presentation.folder.folderhome
+
+data class FolderHomeUiState(
+    val folderName: String = "",
+    val folderCount: Int = 0,
+)
--- app/src/main/java/com/example/kakaotalk/presentation/folder/folderhome/FolderHomeViewModel.kt
+++ app/src/main/java/com/example/kakaotalk/presentation/folder/folderhome/FolderHomeViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.kakaotalk.presentation.folder.folderhome
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class FolderHomeViewModel : ViewModel() {
 
+    private val _uiState = MutableStateFlow(
+    FolderHomeUiState(
+            folderName = "기계",
+            folderCount = 9,
+        )
+    )
+
+    val uiState = _uiState.asStateFlow()
 }
